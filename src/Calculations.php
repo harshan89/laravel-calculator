1df1f1f81b27c2f93ec37e24e27e0dbcdc11fdef
harshanmo <harshan.morawaka@qualitapps.com>
2019-08-29 11:24:10 +0530

    first comming

---
+++ src/Calculations.php
@@ -0,0 +1,21 @@
+<?php 
+
+/**
+ * this going to be and small class for testing the library
+ um not gonna extentds anything here
+ */
+namespace Devdojo\Calculator;
+
+class Calculations
+{
+	
+	function __construct()
+	{
+		
+	}
+
+	public static function test()
+    {
+        return 'Hi man calculator package has loaded!';
+    }
+}
